ae5e562a488de80c0e3adbe17674de9b47dc709b
alvianrml <alvianrmli@gmail.com>
2024-10-28 21:53:14 +0700

    add activity2

--- app/src/main/java/com/example/uts_lab4_alvian/Activity2.kt
+++ app/src/main/java/com/example/uts_lab4_alvian/Activity2.kt
@@ -1,33 +1,20 @@
 package com.example.uts_lab4_alvian
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.FrameLayout
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
-class Activity1 : AppCompatActivity() {
+class Activity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_1)
+        setContentView(R.layout.activity_2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val f1: FrameLayout = findViewById(R.id.f1)
-
-        // Set OnClickListener untuk f1
-        f1.setOnClickListener {
-            // Intent untuk berpindah ke Activity2
-            val intent = Intent(this@Activity1, Activity2::class.java)
-            startActivity(intent)
         }
     }
 }
